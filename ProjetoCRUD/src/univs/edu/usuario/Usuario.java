cfc18908bbcb72b914622260062ddbe83fec9eab
Mikael Menezes <fr3shtm@gmail.com>
2019-11-05 20:56:20 -0200

    CRUD Inicial

---
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -0,0 +1,56 @@
+
+package univs.edu.usuario;
+
+
+public class Usuario {
+    
+public int idUsuario;
+
+public String nomeUsuario;
+
+public String login;
+
+public String senha;
+
+    
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    
+    public String getLogin() {
+        return login;
+    }
+
+    
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    
+    public String getSenha() {
+        return senha;
+    }
+
+    
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+}
